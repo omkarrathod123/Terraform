5c7e3c8c310fb3ab099b502bc7fc9c78abc3f536
omkarrathod123 <omkarrathod0705@gmail.com>
2025-04-24 12:29:58 +0530

    deleting elb

--- ELB-with-ASG/template.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_launch_template" "nginx" {
-  name_prefix = "nginxUbuntu"
-  image_id = var.image_id_ubuntu
-  instance_type = "t2.micro"
-  key_name = "Practic"
-  security_group_names = [ "0mK4r" ]
-}
--- Terraform_Modul/elb/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update -y
-sudo apt install nginx -y
-sudo systemctl start nginx
-
-sudo cd /etc/www/html/
-sudo echo "This is test" > index.html
