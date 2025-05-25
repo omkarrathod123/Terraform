d7fe6aa340ae41fdf9c5422a116b5679bc8daba0
omkarrathod123 <omkarrathod0705@gmail.com>
2025-05-25 16:31:58 +0530

    Updating working terraform code for EKS

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -1,45 +1,13 @@
-resource "aws_eks_cluster" "nginx-Cluster" {
-  name = "Nginx-Cluster"
-
-  access_config {
-    authentication_mode = "API"
-  }
-
-  role_arn = var.cluster_role_arn
-  version  = var.eksversion
-
-  bootstrap_self_managed_addons = false
-
-  compute_config {
-    enabled       = true
-    node_pools    = ["general-purpose"]
-    node_role_arn = var.node_role_arn
-  }
-
-  kubernetes_network_config {
-    elastic_load_balancing {
-      enabled = true
-    }
-  }
-
-  storage_config {
-    block_storage {
-      enabled = true
-    }
-  }
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    endpoint_private_access = true
+    subnet_ids              = [aws_subnet.publicSUB.id,aws_subnet.privateSUB.id,aws_subnet.publicSUB1.id,aws_subnet.privateSUB1.id]
     endpoint_public_access  = true
+  }
 
-    subnet_ids = [
-      "subnet-0098cd6a11d9d9d56",
-      "subnet-0aa2fe8a1c27a5e65",
-      "subnet-04ebafb44c2a9c881"
-    ]
-  }
-  tags = {
-    Name = "nginx-Cluster"
-    Env = "Lab"
-  }
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy_attachment
+  ]
 }
--- EKS/nodegroup.tf
+++ EKS/nodegroup.tf
@@ -1,23 +1,22 @@
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.nginx-Cluster.name
-  node_group_name = "nginx-node-group"
-  node_role_arn   = var.node_role_arn
-  subnet_ids      = [
-      "subnet-0098cd6a11d9d9d56",
-      "subnet-0aa2fe8a1c27a5e65",
-      "subnet-04ebafb44c2a9c881"
-    ]
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.publicSUB.id,aws_subnet.privateSUB.id,aws_subnet.publicSUB1.id,aws_subnet.privateSUB1.id]
+
   scaling_config {
-    desired_size = 3
-    max_size     = 10
-    min_size     = 1
+    desired_size = 2
+    min_size     = 2
+    max_size     = 3
   }
 
-  update_config {
-    max_unavailable = 1
-  }
-  tags = {
-    Name = "Nginx-Node-Group"
-    Env = "Lab"
-  }
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"  # Amazon Linux 2
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_ecr_policy,
+    aws_eks_cluster.eks_cluster
+  ]
 }
