ce7ccd8404108be712de8c6dbf09a6f6ea8cc1ce
omkarrathod123 <omkarrathod0705@gmail.com>
2026-03-03 12:57:18 +0530

    Adding variable.tf file to EC2.

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "practicLinux" {
   ami = "ami-0f2ce9ce760bd7133"
-  instance_type = "t2.micro"
+  instance_type = var.instanceType
   availability_zone = "ap-south-1b"
   key_name = "Practic"
   
   tags = {
     Name = "PracticLinux"
     Env = "Lab"
-    Date = "27022025"
+    Date = "03032026"
   }
 }
---
+++ EC2/variable.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type = string
+  default = "ap-south-1"
+  description = "Default region is mumbai ( ap-south-1 )"
+}
+variable "instanceType" {
+  type = string
+  default = "t3.micro"
+}
