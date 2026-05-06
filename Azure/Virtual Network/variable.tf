864a78a9585e052c2a276eefb874c92d9c8bafa0
omkarrathod123 <omkarrathod0705@gmail.com>
2026-05-06 19:07:26 +0530

    Virtual network template is created!!

--- Azure/Virtual Network/variable.tf
+++ Azure/Virtual Network/variable.tf
@@ -10,3 +10,15 @@ variable "NameNSG" {
   type = string
   default = "Practic-NSG"
 }
+variable "NameVN" {
+  type = string
+  default = "Practic-VNet"
+}
+variable "Tags" {
+  type = map(string)
+  default = {
+    "Resource Group" = "Practic-RG"
+    "Environmment" = "Developement"
+    "Owner" = "Omkar Rathod"
+  }
+}
