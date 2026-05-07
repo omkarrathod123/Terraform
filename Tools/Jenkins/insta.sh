1ab92bd8963fedeefb0cab84da051b17daa6e08e
omkarrathod123 <omkarrathod0705@gmail.com>
2026-05-07 17:57:06 +0530

    Updating jenkins installtion file.

--- Tools/Jenkins/insta.sh
+++ Tools/Jenkins/insta.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-sudo echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt-get update -y
 sudo apt-get install jenkins -y
-sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre -y
-java -version
 
 cd /etc
 echo "jenkins ALL=(ALL) NOPASSWD:ALL" >> sudoers  # To tell system to no passward needed 
 usermod -a -G root jenkins    
 
 sudo systemctl start jenkins
-
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update -y && sudo apt install terraform -y
 
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
