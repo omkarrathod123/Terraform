ec53f909e1e177b506eb63d53dc177296a1b225e
omkarrathod123 <omkarrathod0705@gmail.com>
2026-05-07 11:52:55 +0530

    Resource Group and provider file is created

---
+++ Azure/Virtual Machine/variable.tf
@@ -0,0 +1,9 @@
+variable "namerg" {
+  type = string
+  default = "practic-RG"
+}
+variable "location" {
+  type = string
+  default = "Central India"
+  description = "Default location is Central India."
+}
