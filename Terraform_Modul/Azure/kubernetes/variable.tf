c793e017c2426ca4a261c6886365f6cb97cbb42c
omkarrathod123 <omkarrathod0705@gmail.com>
2026-05-28 10:19:03 +0530

    Kubernetes module is added to demo

---
+++ Terraform_Modul/Azure/kubernetes/variable.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  type = string
+  default = "practic"
+}
+variable "location" {
+  type = string
+  default = "Central India"
+  description = "Default location is Central India."
+}
+variable "tags" {
+  type = map(string)
+  default = {
+    Name = "Practic"
+    Env = "Dev"
+    Resource-Group = "Practic-RG"
+  }
+}
+variable "service_principal" {
+  type = list(string)
+  default = [ "000000-0000-000000-000000", "0000000000000000000000000000000" ]
+}
+variable "vm_size" {
+  type = string
+  default = "Standard_D2_v2"
+  description = "Default VM size is Standard_D2_v2"
+}
