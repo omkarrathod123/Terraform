45297e66486ef370b57b1dc38d2639c644f80c3d
omkarrathod123 <omkarrathod0705@gmail.com>
2026-06-18 13:48:01 +0530

    Creating Base for Azure Cosmodb

---
+++ Azure/Azure-Cosmodb/variable.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type = string
+  default = "practic"
+}
+variable "location" {
+  type = string
+  default = "Central India"
+  description = "Default location is Central India."
+}
